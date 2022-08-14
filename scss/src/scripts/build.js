24b8fc0fbabc6aa717b62d76506cc91138dbcba8
victory-a <victoryasokomeh@gmail.com>
2022-08-14 02:42:43 +0100

    compile css script

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,36 @@
+const Fs = require('fs');
+const Path = require('path');
+const Sass = require('node-sass');
+
+const getComponents = () => {
+  let allComponents = [];
+  const types = ['atoms', 'molecules', 'organisms'];
+
+  types.forEach((type) => {
+    const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
+      input: `src/${type}/${file}`,
+      output: `src/lib/${file.slice(0, -4) + 'css'}`,
+    }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (path, filename) => {
+  const result = Sass.renderSync({
+    data: Fs.readFileSync(Path.resolve(path)).toString(),
+    outputStyle: 'expanded',
+    outFile: 'global.css',
+    includePaths: [Path.resolve('src')],
+  });
+
+  Fs.writeFileSync(Path.resolve(filename), result.css.toString());
+};
+
+compile('src/global.scss', 'src/lib/global.css');
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output);
+});
